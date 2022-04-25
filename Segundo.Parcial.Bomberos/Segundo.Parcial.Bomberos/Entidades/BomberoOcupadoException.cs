df0e93e660130664eaf5b9a198ed87df4778276e
Ramiro Olicino <olicino@gmail.com>
2022-04-25 15:38:53 -0300

    .

---
+++ Segundo.Parcial.Bomberos/Segundo.Parcial.Bomberos/Entidades/BomberoOcupadoException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class BomberoOcupadoException : Exception
+    {
+        public BomberoOcupadoException()
+        {
+        }
+
+        public BomberoOcupadoException(string message)
+            : base(message)
+        {
+        }
+
+        public BomberoOcupadoException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ Segundo.Parcial.Bomberos/Segundo.Parcial.Bomberos/Entidades/LogDAO.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class LogDAO
+    {
+        private string connectionString;
+        private SqlCommand command;
+        private SqlConnection connection;
+
+
+        public LogDAO()
+        {
+            this.connectionString = @"Data Source=.\MSSQLSERVER04; Initial Catalog=bomberos-db;Integrated Security=True";
+            this.command = new SqlCommand();
+            this.connection = new SqlConnection(connectionString);
+            command.Connection = connection;
+            command.CommandType = System.Data.CommandType.Text;
+        }
+
+        public void Insertar(string log)
+        {
+            try
+            {
+                connection.Open();
+                command.CommandText = $"INSERT INTO log(entrada, alumno) VALUES(@entrada, 'Ramiro olicino')";
+                command.Parameters.AddWithValue("@entrada", log);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public string Leer()
+        {
+            try
+            {
+                connection.Open();
+                command.CommandText = "SELECT entrada FROM log";
+                SqlDataReader reader = command.ExecuteReader();
+                StringBuilder sb = new StringBuilder();
+
+                while (reader.Read())
+                {
+                    string log = reader.GetString(0);
+                    sb.AppendLine(log);
+                }
+
+                return sb.ToString();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+    }
+}
